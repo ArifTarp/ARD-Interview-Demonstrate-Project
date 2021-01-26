074d736c0f8bee7f1b11e0f69dd2d90a3f3147a2
ArifTarp <arifoklh@gmail.com>
2021-01-26 14:42:53 +0300

    Added student update dto

---
+++ ARD/ARD.API/DTOs/StudentUpdateDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ARD.API.DTOs
+{
+    public class StudentUpdateDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string SchoolIdentity { get; set; }
+
+        public int AddressId { get; set; }
+    }
+}
