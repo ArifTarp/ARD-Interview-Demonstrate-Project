863161a4a46fb71612d537ae188d6ed230c90f94
ArifTarp <arifoklh@gmail.com>
2021-01-26 14:26:31 +0300

    Addded abstract services

---
+++ ARD/ARD.Business/Abstract/IAddressService.cs
@@ -0,0 +1,21 @@
+﻿using ARD.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARD.Business.Abstract
+{
+    public interface IAddressService
+    {
+        Task<ICollection<Address>> GetAllAsync();
+
+        Task<Address> GetAddressByIdAsync(int id);
+
+        Task AddAddressAsync(Address address);
+
+        Task DeleteAddressAsync(int id);
+
+        Task UpdateAddressAsync(Address address);
+    }
+}
---
+++ ARD/ARD.Business/Abstract/IStudentService.cs
@@ -0,0 +1,21 @@
+﻿using ARD.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARD.Business.Abstract
+{
+    public interface IStudentService
+    {
+        Task<ICollection<Student>> GetAllAsync();
+
+        Task<Student> GetStudentByIdAsync(int id);
+
+        Task AddStudentAsync(Student student);
+
+        Task DeleteStudentAsync(int id);
+
+        Task UpdateStudentAsync(Student student);
+    }
+}
