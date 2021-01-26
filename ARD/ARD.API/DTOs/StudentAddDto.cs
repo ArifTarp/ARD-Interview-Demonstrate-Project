457b59fc7927a7c0a403d96323e072883b35d0f3
ArifTarp <arifoklh@gmail.com>
2021-01-26 14:40:56 +0300

    Added student add dto

---
+++ ARD/ARD.API/DTOs/StudentAddDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ARD.API.DTOs
+{
+    public class StudentAddDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string SchoolIdentity { get; set; }
+
+        public int AddressId { get; set; }
+
+    }
+}
