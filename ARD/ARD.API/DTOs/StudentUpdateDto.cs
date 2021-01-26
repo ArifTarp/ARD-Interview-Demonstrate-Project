d213648c3d1d9d8c26a64edf3291e4e4e05b955b
ArifTarp <arifoklh@gmail.com>
2021-01-26 15:47:26 +0300

    Added id

--- ARD/ARD.API/DTOs/StudentUpdateDto.cs
+++ ARD/ARD.API/DTOs/StudentUpdateDto.cs
@@ -7,6 +7,7 @@ namespace ARD.API.DTOs
 {
     public class StudentUpdateDto
     {
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string SchoolIdentity { get; set; }
