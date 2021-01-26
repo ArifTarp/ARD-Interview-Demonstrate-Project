849f96dfad012bbbf508d17a4b75eb94d6ba8692
ArifTarp <arifoklh@gmail.com>
2021-01-26 14:47:38 +0300

    Added student controller

---
+++ ARD/ARD.API/Controllers/StudentController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ARD.API.DTOs;
+using ARD.Business.Abstract;
+using ARD.Entity.Concrete;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ARD.API.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly IStudentService _studentService;
+        private readonly IMapper _mapper;
+
+
+        public StudentController(IStudentService studentService, IMapper mapper)
+        {
+            _studentService = studentService;
+            _mapper = mapper;
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Get()
+        {
+            var students = await _studentService.GetAllAsync();
+
+            return Ok(students);
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var student = await _studentService.GetStudentByIdAsync(id);
+            if (student == null)
+                return BadRequest("Uncorrected id.");
+
+            return Ok(student);
+        }
+
+        [HttpDelete("delete")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _studentService.DeleteStudentAsync(id);
+            return Ok();
+        }
+
+        [HttpPost("add")]
+        public async Task<IActionResult> Add([FromBody] StudentAddDto studentAddDto)
+        {
+            if (studentAddDto == null)
+                return BadRequest();
+
+            var newStudent = _mapper.Map<Student>(studentAddDto);
+            await _studentService.AddStudentAsync(newStudent);
+
+            return Ok();
+        }
+
+        [HttpPut("update")]
+        public async Task<IActionResult> Update(StudentUpdateDto studentUpdateDto)
+        {
+            if (studentUpdateDto == null)
+                return BadRequest();
+
+            var newStudent = _mapper.Map<Student>(studentUpdateDto);
+            await _studentService.UpdateStudentAsync(newStudent);
+            return Ok();
+        }
+    }
+}
