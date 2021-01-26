f50151c2bb776688d72842afdafcd0278012ba59
ArifTarp <arifoklh@gmail.com>
2021-01-26 14:30:48 +0300

    Added business concretes

---
+++ ARD/ARD.Business/Concrete/AddressManager.cs
@@ -0,0 +1,45 @@
+﻿using ARD.Business.Abstract;
+using ARD.DataAccess.Abstract;
+using ARD.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARD.Business.Concrete
+{
+    public class AddressManager : IAddressService
+    {
+        private readonly IAddressDal _addressDal;
+
+        public AddressManager(IAddressDal addressDal)
+        {
+            _addressDal = addressDal;
+        }
+
+        public async Task<ICollection<Address>> GetAllAsync()
+        {
+            return await _addressDal.GetListAsync();
+        }
+
+        public async Task<Address> GetAddressByIdAsync(int id)
+        {
+            return await _addressDal.GetAsync(a => a.Id == id);
+        }
+
+        public async Task AddAddressAsync(Address address)
+        {
+            await _addressDal.AddAsync(address);
+        }
+
+        public async Task DeleteAddressAsync(int id)
+        {
+            await _addressDal.DeleteAsync(new Address { Id = id });
+        }
+
+        public async Task UpdateAddressAsync(Address address)
+        {
+            await _addressDal.UpdateAsync(address);
+        }
+    }
+}
---
+++ ARD/ARD.Business/Concrete/StudentManager.cs
@@ -0,0 +1,45 @@
+﻿using ARD.Business.Abstract;
+using ARD.DataAccess.Abstract;
+using ARD.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARD.Business.Concrete
+{
+    public class StudentManager : IStudentService
+    {
+        private readonly IStudentDal _studentDal;
+
+        public StudentManager(IStudentDal studentDal)
+        {
+            _studentDal = studentDal;
+        }
+
+        public async Task<ICollection<Student>> GetAllAsync()
+        {
+            return await _studentDal.GetListAsync();
+        }
+
+        public async Task<Student> GetStudentByIdAsync(int id)
+        {
+            return await _studentDal.GetAsync(s => s.Id == id);
+        }
+
+        public async Task AddStudentAsync(Student student)
+        {
+            await _studentDal.AddAsync(student);
+        }
+
+        public async Task DeleteStudentAsync(int id)
+        {
+            await _studentDal.DeleteAsync(new Student { Id = id });
+        }
+
+        public async Task UpdateStudentAsync(Student student)
+        {
+            await _studentDal.UpdateAsync(student);
+        }
+    }
+}
