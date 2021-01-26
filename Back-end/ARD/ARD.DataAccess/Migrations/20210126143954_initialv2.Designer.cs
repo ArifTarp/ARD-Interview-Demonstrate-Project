68299dc9c18e4f736b1330d5ce38ecf9130f9dee
ArifTarp <arifoklh@gmail.com>
2021-01-26 17:44:29 +0300

    Added second migration

--- Back-end/ARD/ARD.DataAccess/Migrations/20210126143954_initialv2.Designer.cs
+++ Back-end/ARD/ARD.DataAccess/Migrations/20210126143954_initialv2.Designer.cs
@@ -3,14 +3,16 @@ using ARD.DataAccess.Concrete.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ARD.DataAccess.Migrations
 {
     [DbContext(typeof(ARDDataContext))]
-    partial class ARDDataContextModelSnapshot : ModelSnapshot
+    [Migration("20210126143954_initialv2")]
+    partial class initialv2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
