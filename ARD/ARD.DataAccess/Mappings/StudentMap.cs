2980a869a362251ffabcea701fd3ec2327b09365
ArifTarp <arifoklh@gmail.com>
2021-01-26 14:23:07 +0300

    Adding mapping of entities

---
+++ ARD/ARD.DataAccess/Mappings/StudentMap.cs
@@ -0,0 +1,20 @@
+﻿using ARD.Entity.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ARD.DataAccess.Mappings
+{
+    public class StudentMap : IEntityTypeConfiguration<Student>
+    {
+        public void Configure(EntityTypeBuilder<Student> builder)
+        {
+            builder.ToTable("Students");
+            builder.HasKey(s => s.LastName);
+
+            builder.HasOne(s => s.Address).WithMany(a => a.Students).HasForeignKey(s => s.AddressId);
+        }
+    }
+}
