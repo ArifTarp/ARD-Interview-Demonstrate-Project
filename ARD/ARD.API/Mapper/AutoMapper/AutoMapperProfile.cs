aa33ce992a716799c4ae651f6d8a7f2021fe5763
ArifTarp <arifoklh@gmail.com>
2021-01-26 14:45:42 +0300

    Added mapper

---
+++ ARD/ARD.API/Mapper/AutoMapper/AutoMapperProfile.cs
@@ -0,0 +1,20 @@
+﻿using ARD.API.DTOs;
+using ARD.Entity.Concrete;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ARD.API.Mapper.AutoMapper
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<StudentAddDto, Student>().ReverseMap();
+            CreateMap<StudentUpdateDto, Student>().ReverseMap();
+
+        }
+    }
+}
