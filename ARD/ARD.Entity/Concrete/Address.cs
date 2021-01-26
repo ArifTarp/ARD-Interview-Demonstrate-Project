672ba45505f36ec9f3b5f40f2b4f6bf2df7e8973
ArifTarp <arifoklh@gmail.com>
2021-01-26 14:22:52 +0300

    Added collection property

--- ARD/ARD.Entity/Concrete/Address.cs
+++ ARD/ARD.Entity/Concrete/Address.cs
@@ -7,9 +7,16 @@ namespace ARD.Entity.Concrete
 {
     public class Address : IEntity
     {
+        public Address()
+        {
+            Students = new List<Student>();
+        }
+
         public int Id { get; set; }
         public string Province { get; set; }
         public string District { get; set; }
+
+        public ICollection<Student> Students { get; set; }
 
     }
 }
