a05d432bb15388a08431392d0c05ca335cefdbbd
ArifTarp <arifoklh@gmail.com>
2021-01-26 21:47:28 +0300

    Added http client module

--- Front-end/ARD/src/app/app.module.ts
+++ Front-end/ARD/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { SharedModule } from './pages/shared.module';
 
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
