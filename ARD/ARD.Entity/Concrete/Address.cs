fd2208c81a024879af90f2e01744b330dc2371f6
ArifTarp <arifoklh@gmail.com>
2021-01-26 13:35:09 +0300

    Added entities

---
+++ ARD/ARD.Entity/Concrete/Address.cs
@@ -0,0 +1,15 @@
+﻿using ARD.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ARD.Entity.Concrete
+{
+    public class Address : IEntity
+    {
+        public int Id { get; set; }
+        public string Province { get; set; }
+        public string District { get; set; }
+
+    }
+}
---
+++ ARD/ARD.Entity/Concrete/Student.cs
@@ -0,0 +1,17 @@
+﻿using ARD.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ARD.Entity.Concrete
+{
+    public class Student : IEntity
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string SchoolIdentity { get; set; }
+        public int AddressId { get; set; }
+
+    }
+}
