4de2cacacf57fc808401abb1ed9e0b98afcd782d
ArifTarp <arifoklh@gmail.com>
2021-01-26 18:44:55 +0300

    Added student page

---
+++ Front-end/ARD/src/app/pages/student-form/student-form.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { StudentFormRoutingModule } from './student-form-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    StudentFormRoutingModule
+  ]
+})
+export class StudentFormModule { }
