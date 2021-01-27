f822aa856e55271c8ad3a3116bfbcef898320a6f
ArifTarp <arifoklh@gmail.com>
2021-01-27 19:29:13 +0300

    Added new route

--- Front-end/ARD/src/app/app-routing.module.ts
+++ Front-end/ARD/src/app/app-routing.module.ts
@@ -3,9 +3,22 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'addStudent',
     loadChildren: () => import('./pages/student-form/student-form.module').then(m => m.StudentFormModule),
-  }
+  },
+  {
+    path: 'listStudent',
+    loadChildren: () => import('./pages/student-list/student-list.module').then(m => m.StudentListModule),
+  },
+  {
+    path: 'addAddress',
+    loadChildren: () => import('./pages/address-form/address-form.module').then(m => m.AddressFormModule),
+  },
+  {
+    path: 'listAddress',
+    loadChildren: () => import('./pages/address-list/address-list.module').then(m => m.AddressListModule),
+  },
+  {path: '', redirectTo: '/addStudent', pathMatch: 'full'}
 ];
 
 @NgModule({
