302a22e5a4d7fe4883e75e951cee4c52345d9ce7
ArifTarp <arifoklh@gmail.com>
2021-01-27 19:46:26 +0300

    Added new operation in data access layer

--- Back-end/ARD/ARD.DataAccess/Abstract/IStudentDal.cs
+++ Back-end/ARD/ARD.DataAccess/Abstract/IStudentDal.cs
@@ -2,11 +2,14 @@
 using ARD.Entity.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ARD.DataAccess.Abstract
 {
     public interface IStudentDal : IEntityRepository<Student>
     {
+        Task<List<Student>> GetStudentsWithAddresses(Expression<Func<Student, bool>> filter = null);
     }
 }
