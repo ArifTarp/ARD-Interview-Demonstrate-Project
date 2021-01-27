3604fc33c704001eb0465b361e6287949370afb3
ArifTarp <arifoklh@gmail.com>
2021-01-27 19:32:51 +0300

    Added data access operations

--- Back-end/ARD/ARD.DataAccess/Concrete/EntityFrameworkCore/EfAddressDal.cs
+++ Back-end/ARD/ARD.DataAccess/Concrete/EntityFrameworkCore/EfAddressDal.cs
@@ -1,13 +1,15 @@
 ﻿using ARD.Core.DataAccess.EntityFrameworkCore;
 using ARD.DataAccess.Abstract;
 using ARD.Entity.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ARD.DataAccess.Concrete.EntityFrameworkCore
 {
-    public class EfAddressDal : EfEntityRepositoryBase<Address,ARDDataContext>,IAddressDal
+    public class EfAddressDal : EfEntityRepositoryBase<Address, ARDDataContext>, IAddressDal
     {
     }
 }
--- Back-end/ARD/ARD.DataAccess/Concrete/EntityFrameworkCore/EfProvinceDal.cs
+++ Back-end/ARD/ARD.DataAccess/Concrete/EntityFrameworkCore/EfProvinceDal.cs
@@ -1,13 +1,26 @@
 ﻿using ARD.Core.DataAccess.EntityFrameworkCore;
 using ARD.DataAccess.Abstract;
 using ARD.Entity.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ARD.DataAccess.Concrete.EntityFrameworkCore
 {
     public class EfProvinceDal : EfEntityRepositoryBase<Province, ARDDataContext>, IProvinceDal
     {
+        public async Task<List<Province>> GetListWithDistrictsAsync(Expression<Func<Province, bool>> filter = null)
+        {
+            using (var context = new ARDDataContext())
+            {
+                if (filter == null)
+                    return await context.Set<Province>().Include(p => p.Districts).ToListAsync();
+                return await context.Set<Province>().Include(p => p.Districts).Where(filter).ToListAsync();
+            }
+        }
     }
 }
