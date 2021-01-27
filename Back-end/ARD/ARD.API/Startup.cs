28a97ffe28ec96ef17100f2c5bf574a7bf9c51a5
ArifTarp <arifoklh@gmail.com>
2021-01-27 19:21:37 +0300

    Added newtonsoftjson configs

--- Back-end/ARD/ARD.API/Startup.cs
+++ Back-end/ARD/ARD.API/Startup.cs
@@ -63,6 +63,8 @@ namespace ARD.API
                         .AllowAnyMethod()
                         .AllowAnyHeader());
             });
+
+            services.AddMvc().AddNewtonsoftJson(opt => opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
