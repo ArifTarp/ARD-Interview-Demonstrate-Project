a7f5335bea1bd17c7d18cfbd3dd7ccbe288aa487
ArifTarp <arifoklh@gmail.com>
2021-01-27 19:35:00 +0300

    Changed student form

--- Front-end/ARD/src/app/pages/student-form/student-form.component.ts
+++ Front-end/ARD/src/app/pages/student-form/student-form.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
-import { Student } from './../../models/student';
-import { Address } from './../../models/address';
+import { NewStudent } from './../../models/newStudent';
+import { Province } from './../../models/province';
+
 import { StudentService } from 'src/app/services/student.service';
+import { AddressService } from 'src/app/services/address.service';
+import { District } from 'src/app/models/district';
 
 
 @Component({
@@ -13,14 +16,24 @@ import { StudentService } from 'src/app/services/student.service';
 })
 export class StudentFormComponent implements OnInit {
 
-  student: Student;
+  newStudent: NewStudent;
   registerForm: FormGroup;
-  address: Address;
 
-  constructor(private formBuilder: FormBuilder,private studentService: StudentService) { }
+  provinces: Province[] = [];
+
+  selectedProvince: Province;
+
+  constructor(private formBuilder: FormBuilder, private studentService: StudentService, private addressService: AddressService) { }
 
   ngOnInit(): void {
     this.createRegisterForm();
+    this.getProvincesWithDistricts();
+  }
+
+  getProvincesWithDistricts() {
+    this.addressService.getProvincesWithDistricts().subscribe(data => {
+      this.provinces = data;
+    });
   }
 
   createRegisterForm() {
@@ -28,24 +41,29 @@ export class StudentFormComponent implements OnInit {
       firstName: new FormControl(''),
       lastName: new FormControl(''),
       schoolIdentity: new FormControl(''),
-      address: new FormControl('')
-    }
-    )
+      provinceId: new FormControl(0),
+      districtId: new FormControl(0)
+    })
   }
 
   changeProvince(e) {
-    
-    /*this.fruits.setValue(e.target.value, {
-      onlySelf: true
-    })*/
+    this.selectedProvince = this.provinces.find(p => p.id == e.target.value);
+    this.registerForm.get('provinceId').setValue(parseInt(e.target.value), { onlySelf: true })
   }
 
   changeDistrict(e) {
-    
+    this.registerForm.get('districtId').setValue(parseInt(e.target.value), { onlySelf: true })
   }
 
-  register(){
-    this.student = Object.assign({},this.registerForm.value)
-    this.studentService.addStudent(this.student);
+  register() {
+    this.newStudent = {
+      firstName:this.registerForm.value.firstName,
+      lastName:this.registerForm.value.lastName,
+      schoolIdentity:this.registerForm.value.schoolIdentity,
+      provinceId:parseInt(this.registerForm.value.provinceId),
+      districtId:parseInt(this.registerForm.value.districtId),
+    }
+    
+    this.studentService.addStudent(this.newStudent);
   }
 }
