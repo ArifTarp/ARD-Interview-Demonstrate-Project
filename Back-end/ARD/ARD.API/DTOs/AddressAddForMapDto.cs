3d6a474b4ee070fbb230dd32336e64984f9d37b6
ArifTarp <arifoklh@gmail.com>
2021-01-27 19:21:59 +0300

    Added new dto

---
+++ Back-end/ARD/ARD.API/DTOs/AddressAddForMapDto.cs
@@ -0,0 +1,16 @@
+﻿using ARD.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ARD.API.DTOs
+{
+    public class AddressAddForMapDto
+    {
+        public Province Province { get; set; }
+        public District District { get; set; }
+        public AddressAddDto AddressAddDto { get; set; }
+
+    }
+}
