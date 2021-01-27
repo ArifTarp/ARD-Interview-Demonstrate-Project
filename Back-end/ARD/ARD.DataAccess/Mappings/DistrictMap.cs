8a0fcd3b89bea2e7c358f6988aa45c17eb4aef9a
ArifTarp <arifoklh@gmail.com>
2021-01-27 19:27:39 +0300

    Added new map of district

---
+++ Back-end/ARD/ARD.DataAccess/Mappings/DistrictMap.cs
@@ -0,0 +1,22 @@
+﻿using ARD.Entity.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ARD.DataAccess.Mappings
+{
+    public class DistrictMap : IEntityTypeConfiguration<District>
+    {
+        public void Configure(EntityTypeBuilder<District> builder)
+        {
+            builder.ToTable("Districts");
+            builder.HasKey(d => d.Id);
+
+            builder.HasOne(d => d.Province).WithMany(p => p.Districts).HasForeignKey(d => d.provinceId).OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasIndex(d => new { d.Name, d.provinceId }).IsUnique();
+        }
+    }
+}
