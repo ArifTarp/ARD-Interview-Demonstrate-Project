54c6beaa7a073d15260dcc8676e57852f9c12585
ArifTarp <arifoklh@gmail.com>
2021-01-28 16:34:06 +0300

    Added operations for update address

--- Front-end/ARD/src/app/pages/address-form/address-form.component.ts
+++ Front-end/ARD/src/app/pages/address-form/address-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { AddressService } from 'src/app/services/address.service';
 
 import { NewAddress } from '../../models/newAddress';
@@ -14,10 +15,33 @@ export class AddressFormComponent implements OnInit {
   registerForm: FormGroup;
   newAddress: NewAddress;
 
-  constructor(private addressService: AddressService) { }
+  header:string;
+  buttonText:string;
+
+  constructor(private addressService: AddressService, private route:ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.setValuesByRoute();
+  }
+
+  setValuesByRoute() {
     this.createRegisterForm();
+    var provinceName = this.route.snapshot.paramMap.get('provinceName');
+    var districtName = this.route.snapshot.paramMap.get('districtName');
+    var addressDetail = this.route.snapshot.paramMap.get('addressDetail');
+
+    this.registerForm.get('province').setValue(provinceName, { onlySelf: true })
+    this.registerForm.get('district').setValue(districtName, { onlySelf: true })
+    this.registerForm.get('addressDetail').setValue(addressDetail, { onlySelf: true })    
+
+    if (provinceName && districtName && addressDetail) {
+      this.header = "Update Address";
+      this.buttonText = "Update";
+    }
+    else{
+      this.header = "Add Address";
+      this.buttonText = "Register";
+    }
   }
 
   createRegisterForm() {
