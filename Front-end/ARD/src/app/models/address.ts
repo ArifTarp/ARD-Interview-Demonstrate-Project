9990ac0e4e6dea3e2b8a77362ce0700da1dba56b
ArifTarp <arifoklh@gmail.com>
2021-01-28 16:17:30 +0300

    Added new field

--- Front-end/ARD/src/app/models/address.ts
+++ Front-end/ARD/src/app/models/address.ts
@@ -1,9 +1,11 @@
 import { District } from "./district";
 import { Province } from "./Province";
+import { Student } from "./student";
 
 export class Address {
     id?:number;
     province:Province;
     district:District;
     addressDetail?:string;
+    students:Student[];
 }
