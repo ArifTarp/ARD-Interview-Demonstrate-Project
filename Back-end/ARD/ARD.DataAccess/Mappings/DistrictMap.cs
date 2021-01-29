17c66828ee195412f47a5c60ea0135a30ea62d96
ArifTarp <arifoklh@gmail.com>
2021-01-29 14:11:12 +0300

    Added seed

--- Back-end/ARD/ARD.DataAccess/Mappings/DistrictMap.cs
+++ Back-end/ARD/ARD.DataAccess/Mappings/DistrictMap.cs
@@ -17,6 +17,17 @@ namespace ARD.DataAccess.Mappings
             builder.HasOne(d => d.Province).WithMany(p => p.Districts).HasForeignKey(d => d.provinceId).OnDelete(DeleteBehavior.Restrict);
 
             builder.HasIndex(d => new { d.Name, d.provinceId }).IsUnique();
+
+            builder.HasData(
+                new District { Name = "Yenimahalle", provinceId = 1},
+                new District { Name = "Çankaya", provinceId = 1 },
+                new District { Name = "Bahçeşehir", provinceId = 2 },
+                new District { Name = "Beyoğlu", provinceId = 2 },
+                new District { Name = "Osmangazi", provinceId = 3 },
+                new District { Name = "İnegöl", provinceId = 3 },
+                new District { Name = "Selçuk", provinceId = 4 },
+                new District { Name = "Seferihisar", provinceId = 4 }
+            );
         }
     }
 }
---
+++ Back-end/ARD/ARD.DataAccess/Mappings/ProvinceMap.cs
@@ -0,0 +1,25 @@
+﻿using ARD.Entity.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ARD.DataAccess.Mappings
+{
+    public class ProvinceMap : IEntityTypeConfiguration<Province>
+    {
+        public void Configure(EntityTypeBuilder<Province> builder)
+        {
+            builder.ToTable("Provinces");
+            builder.HasKey(p => p.Id);
+
+            builder.HasData(
+                new Province { Name="Ankara" },
+                new Province { Name = "İstanbul" },
+                new Province { Name = "Bursa" },
+                new Province { Name = "İzmir" }
+            );
+        }
+    }
+}
