e142f6783c0f8385b9961df73bb97aaf804c7921
ArifTarp <arifoklh@gmail.com>
2021-01-30 16:35:55 +0300

    Added result

---
+++ Back-end/ARD/ARD.Core/Utilities/Results/ErrorDataResult.cs
@@ -0,0 +1,20 @@
+﻿using Core.Utilities.Results;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace Common.Utilities.Results
+{
+    public class ErrorDataResult<T> : IDataResult<T>
+    {
+        public T Data { get; set; }
+        public int HttpStatusCode { get; set; }
+
+        public ErrorDataResult(T data, HttpStatusCode httpStatusCode)
+        {
+            this.Data = data;
+            this.HttpStatusCode = Convert.ToInt32(httpStatusCode);
+        }
+    }
+}
